b5fff429384e73caf7105a57566aafa9113d19b6
thevi <thevi@VINCINATOR>
2026-02-24 16:54:46 +0530

    Patterns Addeed

---
+++ striverDSA/src/patterns/Pattern1.java
@@ -0,0 +1,28 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+	
+	
+	public static void pattern(int n) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0;i<t;i++) {
+			int j = sc.nextInt();
+			pattern(j);
+		}
+	}
+
+}
---
+++ striverDSA/src/patterns/Pattern5.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class Pattern5 {
+	
+	public static void pattern5(int n) {
+		for(int i=0;i<n;i++) {
+			for(int j=n;j>i;j--) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		pattern5(5);
+	}
+
+}
